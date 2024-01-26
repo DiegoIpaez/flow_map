5170ee0b7a62ef86d7bb4fadb9ebd5447cfd5442
Diegoipaez <diegokpo1313@gmail.com>
2024-01-26 01:29:42 -0300

    built-in flow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import "reactflow/dist/style.css";
+import Flow from "@/components/flow/Flow";
+import Navbar from "@/components/navbar/Navbar";
+import useDarkModeStore from "@/store/darkModeStore";
+
 function App() {
-  return <h1>basic-app</h1>;
+  const { isDarkMode } = useDarkModeStore();
+
+  return (
+    <div className="app-container" data-theme={isDarkMode ? "dark" : "light"}>
+      <Navbar />
+      <div style={{ height: "90%" }}>
+        <Flow />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/flow/Flow.tsx
@@ -0,0 +1,12 @@
+import ReactFlow, { Controls, Background } from "reactflow";
+
+const Flow = () => {
+  return (
+    <ReactFlow>
+      <Background />
+      <Controls />
+    </ReactFlow>
+  );
+};
+
+export default Flow;
